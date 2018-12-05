30db75bfcea63883189c54f2f7a2146a89d49df3
Daniel Wippermann <Daniel.Wippermann@gmail.com>
2018-12-05 21:30:03 +0100

    Allow deactiving `TickSource` using an interval of zero.

--- src/tick_source.rs
+++ src/tick_source.rs
@@ -9,22 +9,32 @@ pub struct TickSource {
 
 impl TickSource {
     pub fn new(interval: i64, now: DateTime<UTC>) -> TickSource {
+        let last_interval = if interval > 0 {
+            now.timestamp() / interval
+        } else {
+            0
+        };
+
         TickSource {
             interval,
-            last_interval: now.timestamp() / interval,
+            last_interval,
         }
     }
 
     pub fn process(&mut self, now: DateTime<UTC>) -> bool {
-        let current_interval = now.timestamp() / self.interval;
+        if self.interval > 0 {
+            let current_interval = now.timestamp() / self.interval;
 
-        let diff = current_interval - self.last_interval;
+            let diff = current_interval - self.last_interval;
 
-        let ticked = (diff > 0) || (diff < -1);
-        if ticked {
-            self.last_interval = current_interval;
+            let ticked = (diff > 0) || (diff < -1);
+            if ticked {
+                self.last_interval = current_interval;
+            }
+
+            ticked
+        } else {
+            false
         }
-
-        ticked
     }
 }
